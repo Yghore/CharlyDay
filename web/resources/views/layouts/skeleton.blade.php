1ad4d21c4934b8f4ba8ce28d468bc5bc68d5d45a
Yghore <Yghore@gmail.com>
2023-02-16 16:53:13 +0100

    Skeleton

---
+++ web/resources/views/layouts/skeleton.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--  Links  -->
+    <link rel="stylesheet" href="storage/{{ asset('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@500&family=Martel&display=swap" rel="stylesheet">
+
+    <title>Home - @yield('title', 'HOme')</title>
+</head>
+<body>
+<!--  Header  -->
+<header>
+    <div class="navbar">
+        <ul>
+            <li><a href="{{ route('home') }}">Accueil</a></li>
+            <li><a href="">A propos</a></li>
+            <li><a href="">Catalogue</a></li>
+            <li><a href="">Panier</a></li>
+            <li><a href="">Connexion</a></li>
+        </ul>
+    </div>
+</header>
+
+<!--  Main  -->
+<main>
+    <!--  Background Image  -->
+    <div class="background-image">
+        <div class="vignette"></div>
+        <div class="background-image__text">
+            <h2>Bienvenue <br> à <br> court-circuit Nancy</h2>
+            <h3>le local à vivre</h3>
+        </div>
+    </div>
+
+    <!--  Catalog  -->
+    <div class="container">
+        @yield('app')
+    </div>
+</main>
+</body>
+</html>
--- web/resources/views/test.blade.php
+++ web/resources/views/test.blade.php
@@ -1,3 +1,12 @@
-coucou
+@extends('layouts.skeleton');
 
-{!! $test !!}
+
+@section('title', 'Page de test')
+
+@section('app')
+
+
+    <h1>fsdjlfdsjlfdsjfds Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus animi consequatur deleniti deserunt distinctio dolore ea, illo, labore modi possimus, soluta tempore veniam? Aperiam aspernatur doloribus modi nisi soluta.</h1>
+
+
+@endsection
