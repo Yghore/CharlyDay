18e96ab09e2c10ff24706183820f405db5750755
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-02-16 18:04:27 +0100

    oublie d'ajout pour le merge

---
+++ web/app/Http/Controllers/ProductController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index($id)
+    {
+        $products = Product::get()->where('product_id', $id);
+        return view('products', ['products' => $products]);
+    }
+
+
+    public function addProductToCart(Request $request, $id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            url()->previous();
+        } else {
+            if ($request->session()->has('carts')) {
+                //add the id to the table carts exist in session
+                $carts = $request->session()->get('carts');
+            } else {
+                //create the table carts in session
+                $carts = collect([]);
+            }
+            $carts->push($id);
+            $request->session()->put('carts', $carts);
+            return redirect()->route('product', ['id' => $id]);
+
+        }
+
+
+    }
+
+}
