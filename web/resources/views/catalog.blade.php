6470b32492a39a3d8cab492ccd166f66b0b36d2c
Luquor <signorellilucas7@gmail.com>
2023-02-16 21:25:16 +0100

    Nouveau style pour les articles 🕺

--- web/resources/views/catalog.blade.php
+++ web/resources/views/catalog.blade.php
@@ -18,22 +18,26 @@
                 <img src="{{ '/storage/img/'.$product->product_id.'.jpg' }}" alt="{{ $product->nom }}">
             </div>
 
-            <h3 class="product_title">{{ $product->nom }}</h3>
-
-            <p class="dispo">Disponible à {{ $product->lieu }}</p>
-            @if($product->poids == 0)
-                <p id="price">{{ $product->prix}}€/kg</p>
-            @else
-                <p id="price">{{ $product->prix}}€</p>
-            @endif
+            <div class="infos">
+                <h3 class="product_title">{{ $product->nom }}</h3>
+                <span class="dispo">Disponible à {{ $product->lieu }}</span>
+                @if($product->poids == 0)
+                    <p id="price">{{ $product->prix}}€/kg</p>
+                @else
+                    <p id="price">{{ $product->prix}}€</p>
+                @endif
+            </div>
         </div>
     @endforeach
+
+    <div class="pagination">
     @if ($products->currentPage() > 1)
         <a href="{{ $products->previousPageUrl() }}">Page précédente</a>
     @endif
     @if ($products->hasMorePages())
         <a href="{{ $products->nextPageUrl() }}">Page suivante</a>
     @endif
+    </div>
 </div>
 
 </body>
