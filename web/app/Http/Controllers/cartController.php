ff0e0145206b57f870ad48a410784b7af56db2fe
ismael <ismael.kone4@etu.univ-lorraine.fr>
2023-02-16 17:17:44 +0100

    cartController: cart validation, cart validation management and database update

---
+++ web/app/Http/Controllers/cartController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class cartController extends Controller
+{
+    public function validateCart(Request $request)
+    {
+        $carts = $request->session()->get('carts');
+        $products = Product::whereIn('id', $carts);
+        $total = 0;
+        foreach ($products as $product) {
+            $total += $product->price;
+        }
+        return view('validate_cart', ['products', 'total']);
+    }
+
+    public function confirmOrder(Request $request)
+    {
+
+        // Get the products in the carts
+        $carts = session('carts', []);
+        
+
+        // Clear the carts$carts
+        $request->session()->put('carts', collect([]));
+
+        // Redirect to confirmation page
+        return view('order_confirmation');
+    }
+}
+
