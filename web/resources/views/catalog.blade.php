0f2637446658f42d567dbb67566235dbf9ada9a3
Luquor <signorellilucas7@gmail.com>
2023-02-16 18:50:09 +0100

    Mise en place du CSS (changement noms des classes et id)

--- web/resources/views/catalog.blade.php
+++ web/resources/views/catalog.blade.php
@@ -1,15 +1,40 @@
-<div id="catalog">
-    @foreach($products as $product)
-    <div class="products">
-        <img src="{{ '/storage/img/'.$product->product_id.'.jpg' }}" alt="{{ $product->nom }}">
-        <h2>{{ $product->nom }}</h2>
-        <p>Disponible à {{ $product->lieu }}</p>
-        @if($product->poids == 0)
-            <p>{{ $product->prix}}€/kg</p>
-        @else
-            <p>{{ $product->prix}}€</p>
-        @endif
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="storage/{{ asset('css/app.css') }}">
+
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="cards">
+        @foreach($products as $product)
+            <div class="card">
+                <div class="image">
+                    <img src="{{ '/storage/img/'.$product->product_id.'.jpg' }}" alt="{{ $product->nom }}">
+                </div>
+
+                <h3 class="product_title">{{ $product->nom }}</h3>
+
+                <p class="dispo">Disponible à {{ $product->lieu }}</p>
+                @if($product->poids == 0)
+                    <p id="price">{{ $product->prix}}€/kg</p>
+                @else
+                    <p id="price">{{ $product->prix}}€</p>
+                @endif
+            </div>
+      @endforeach
     </div>
-    @endforeach
-</div>
+
+</body>
+</html>
+
+
+
+
 
