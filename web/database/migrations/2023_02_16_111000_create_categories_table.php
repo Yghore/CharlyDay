6ca129919850ba2f3a834bd9971c3969a103160f
Yghore <Yghore@gmail.com>
2023-02-16 16:44:28 +0100

    Init db

--- web/database/migrations/2023_02_16_111000_create_categories_table.php
+++ web/database/migrations/2023_02_16_111000_create_categories_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,6 +16,15 @@ return new class extends Migration
             $table->id();
             $table->string('nom');
         });
+
+        DB::table('categorie')->insert(array(
+                ['nom'=>'Epicerie'],
+                ['nom'=>'Boissons'],
+                ['nom'=>'Droguerie'],
+                ['nom'=>'Cosmétique'],
+                ['nom'=>'Produits frais']
+            )
+        );
     }
 
     /**
